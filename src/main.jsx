19f5dc7b54437025e434c300d85277ee1397339b
Viswanath <viswanathviswa999@gmail.com>
2026-06-21 12:33:03 +0530

    chore: configure GitHub Pages deployment for mini-project

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import { AppProvider } from './context/AppContext'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
+    <HashRouter basename="/mini-project">
+      <AuthProvider>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </AuthProvider>
     </HashRouter>
   </StrictMode>,
 )
